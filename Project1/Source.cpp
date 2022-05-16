9146d2c3fb2b8971c92d6018a51d2b48ec607fd9
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2022-05-16 20:02:32 +0530

    added deserialization

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,20 +3,42 @@
 #include <json/value.h>
 #include <fstream>
 #include <string>
+#include <iostream>
+
 
 using namespace std;
+using namespace Json;
 
 int main()
 {
-
+    //serialization part
     ifstream file("data.json");
-    Json::Value actualJson;
-    Json::Reader reader;
-
+    Value actualJson;
+    Reader reader;
+    cout << "Serialization output" << endl;
     reader.parse(file, actualJson);
     cout << actualJson["Name"] << endl;
     cout << actualJson["Age"] << endl;
     cout << actualJson["City"] << endl;
+
+
+    //Deserialization part
+    Value event;
+    
+    event["User"]["Student"]["name"] = "Pratik";
+    event["User"]["Student"]["age"] = 21;
+    event["User"]["Employee"]["name"] = "Mukul";
+    event["User"]["Intern"]["name"] = "Manish";
+    event["User"]["Boss"]["code"] = "Sahil";
+
+    cout << event << endl;
+    ofstream MyFile("output.json");
+
+    // Write to the file
+    MyFile << event;
+
+    // Close the file
+    MyFile.close();
 
     return 0;
 }
